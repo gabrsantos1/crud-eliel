ebcd3fe5190cafefef90ea97befc29e8ddd9c1ff
Gabriel Santos <gs2625020@gmail.com>
2025-06-15 21:44:06 -0300

    feat: add jwt authentication + dto

---
+++ src/main/java/com/example/gbr/dto/SignupRequest.java
@@ -0,0 +1,55 @@
+package com.example.gbr.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import com.example.gbr.model.Role;
+
+public class SignupRequest {
+    @NotBlank
+    @Size(max = 50)
+    private String name;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 6, max = 40)
+    private String password;
+
+    private Role role;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+} 
